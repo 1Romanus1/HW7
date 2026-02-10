ba7b7697a1230aa8a448e0e9c8d03a2f9db82979
Roman <romka.polupan@gmail.com>
2026-02-10 19:32:29 +0300

    HW7 add+

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,13 @@ public class Main {
 
         int[] oWeight = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         for (int in = 0; in < oWeight.length; in++) {
-            System.out.println(oWeight[in]);
+            System.out.print(oWeight[in]);
+            if (in < oWeight.length - 1) {
+                System.out.print(", ");
+            }
         }
 //Задача 3
+        System.out.println();
         for (int ind = oWeight.length - 1; ind >= 0; ind--) {
             System.out.print(oWeight[ind]);
             if(ind > 0) {
@@ -37,6 +41,7 @@ public class Main {
             }
         }
         //Задача 4
+        System.out.println();
         int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
